0bceee1f8d2e4a1a1f5744f5e220491e7701bc72
Samuel Martinenghi <frontendsam@gmail.com>
2022-05-16 14:17:16 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <Nav />
+    <router-view />
+</template>
+
+<script setup>
+import Nav from './components/nav.vue'
+</script>
+
+<style lang="scss">
+@import './styles/global/global.scss';
+
+@import './styles/global/reset.scss';
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="pageHome">
+        <h1 class="pageHome_title">
+            Home
+        </h1>
+        Clicked: {{ count }} times, count is {{ evenOrOdd }}.
+        <button @click="increment">
+            +
+        </button>
+        <button @click="decrement">
+            -
+        </button>
+        <button @click="incrementIfOdd">
+            Increment if odd
+        </button>
+        <button @click="incrementAsync">
+            Increment async
+        </button>
+    </main>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+    name: 'PageHome',
+    setup() {
+        const store = useStore()
+
+        return {
+            count: computed(() => store.state.count),
+            evenOrOdd: computed(() => store.getters.evenOrOdd),
+            increment: () => store.dispatch('increment'),
+            decrement: () => store.dispatch('decrement'),
+            incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+            incrementAsync: () => store.dispatch('incrementAsync')
+        }
+    }
+}
+
+
+</script>
+
+<style lang="scss">
+</style>
