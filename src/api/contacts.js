fe44ce608eecd0852b6a352d53c152b653bf255e
Samuel <frontendsam@gmail.com>
2022-05-20 19:32:20 -0300

    refactor

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -11,13 +11,11 @@ export default {
     },
     async updateContacts(payload) {
         try {	
-            const response = await new Promise(resolve => {
+            return await new Promise(resolve => {
                 const payloadString = JSON.stringify(payload)
                 localStorage.setItem('contacts', payloadString)	
             	resolve()
             })
-			
-            return response
         } catch (error) {
             console.error(error)
         }
