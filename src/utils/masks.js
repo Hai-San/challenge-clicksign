c0b6d843b7159335862660794be46340ebbf5552
Samuel <frontendsam@gmail.com>
2022-05-20 09:56:41 -0300

    mask phone input

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -1,8 +1,21 @@
 import Inputmask from 'inputmask/dist/inputmask.es6'
 
 class Masks {
+    constructor() {
+        this.phoneMask = '(99) 99999-999[9]'
+    }
+
     formatPhone(phone) {
-        return phone ? Inputmask.format(phone, { mask: '(99) 99999-999[9]' }) : ''
+        return phone ? Inputmask.format(phone, { mask: this.phoneMask }) : ''
+    }
+	
+    inputPhone(input) {
+        Inputmask({ 
+            mask: this.phoneMask,
+            greedy: false,
+            clearIncomplete: true,
+            showMaskOnHover: false
+		 }).mask(input)
     }
 }
 
